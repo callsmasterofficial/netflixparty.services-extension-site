bfd886c6d1eb2afe26dc66a07eb148d78c5b0be5
callsmasterofficia <partnerchc@outlook.com>
2023-07-08 16:39:42 +0530

    fad

--- pages/404.js
+++ pages/404.js
@@ -1,27 +1,30 @@
 import React from 'react'
-import Navbar from "../components/Navbar"
+import Navbar from '../components/Navbar'
 import Error from '../components/404'
 
-export async function getStaticProps(){
+export async function getStaticProps() {
   return {
-    props:{
-      meta:{
-        title:"Netflix Party - Stream Netflix with Your Friends | Install the Extension",
-        description:"Netflix Party allows Netflix subscribers to watch movies and shows together online and connect from various locations.",
-        keywords:"Netflix Party, Netflix Watch Party, Netflix Party Extension, Netflix Party Chrome Extension, Watch Party Netflix, How to do Netflix Party, How to use Netflix Party, How to do a Netflix Party, Netflix Watch Party Extension",
-        pageUrl:"https://www.netflixparty.services/",
-        featuredImage:"/logo.png"
-      }
-    }
+    props: {
+      meta: {
+        title:
+          'Netflix Party - Stream Netflix with Your Friends | Install the Extension',
+        description:
+          'Netflix Party allows Netflix subscribers to watch movies and shows together online and connect from various locations.',
+        keywords:
+          'Netflix Party, Netflix Watch Party, Netflix Party Extension, Netflix Party Chrome Extension, Watch Party Netflix, How to do Netflix Party, How to use Netflix Party, How to do a Netflix Party, Netflix Watch Party Extension',
+        pageUrl: 'https://www.netflixparty.services/',
+        featuredImage: '/logo.png',
+      },
+    },
   }
 }
 function error() {
   return (
-    <div className='error_404 '>
+    <div className="error_404 ">
       <div>
-        <Navbar/>
+        <Navbar />
       </div>
-      <Error/>
+      <Error />
     </div>
   )
 }
--- pages/api/sitemap.js
+++ pages/api/sitemap.js
@@ -1,17 +1,13 @@
-import db from "../../config/db";
-import Blog from "../../models/blog"
-import siteConfig from "../../siteConfig";
+import db from '../../config/db'
+import Blog from '../../models/blog'
+import siteConfig from '../../siteConfig'
 
-
-export default async function handler(
-  req,
-  res
-) {
-  try{
-    await db();
-    const blogs=await Blog.find({site:siteConfig.siteId},'-_id slug');
+export default async function handler(req, res) {
+  try {
+    await db()
+    const blogs = await Blog.find({ site: siteConfig.siteId }, '-_id slug')
     res.send(blogs)
-  }catch(err){
-    res.status(500).json({message:err.message});
+  } catch (err) {
+    res.status(500).json({ message: err.message })
   }
 }
